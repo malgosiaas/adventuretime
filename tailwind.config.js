1f8ab0754593e903c90a27647991da6d20b7fcfc
malgosiaas <m.szeler@student.uw.edu.pl>
2024-03-21 10:56:16 +0100

    kk

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'regal-blue': '#243c5a',
+      },
+    },
   },
   plugins: [],
 }
